cd1cb581b742f18565a3bf17a85d014807b94dc4
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-24 17:07:13 +0100

    Modifiche generali

--- app.js
+++ app.js
@@ -31,64 +31,32 @@ const app = new Vue ({
             }
         ],
 
+        currentIndex: 0,
+
     },
 
     methods: {
 
         buttonDown: function() {
 
-            image[currentIndex].classList.remove('active');
-            imageSlider[currentIndex].classList.remove('active');
-            titleSlider[currentIndex].classList.remove('active');
-            descripionSlider[currentIndex].classList.remove('active');
-
-            if ( currentIndex < image.length - 1 ) {
-                currentIndex++;
+            if ( this.currentIndex < this.carouselSlide.length - 1 ) {
+                this.currentIndex++;
             } else {
-                currentIndex = 0;
+                this.currentIndex = 0;
             }
-
-            image[currentIndex].classList.add('active');
-            imageSlider[currentIndex].classList.add('active');
-            titleSlider[currentIndex].classList.add('active');
-            descripionSlider[currentIndex].classList.add('active');
 
         },
 
         buttonUp: function () {
 
-            image[currentIndex].classList.remove('active');
-            imageSlider[currentIndex].classList.remove('active');
-            titleSlider[currentIndex].classList.remove('active');
-            descripionSlider[currentIndex].classList.remove('active');
-
-            if ( currentIndex > 0 ) {
-                currentIndex--;
+            if ( this.currentIndex > 0 ) {
+                this.currentIndex--;
             } else {
-                currentIndex = this.carouselSlide.length - 1;
+                this.currentIndex = this.carouselSlide.length - 1;
             }
-
-            image[currentIndex].classList.add('active');
-            imageSlider[currentIndex].classList.add('active');
-            titleSlider[currentIndex].classList.add('active');
-            descripionSlider[currentIndex].classList.add('active');
 
         }
 
     }
 
 });
-
-let currentIndex = 0;
-
-const image = [...document.getElementsByClassName('item')];
-image[currentIndex].classList.add('active');
-
-const imageSlider = {...document.getElementsByClassName('item-slide')};
-imageSlider[currentIndex].classList.add('active');
-
-const titleSlider = {...document.getElementsByClassName('title-slider')};
-titleSlider[currentIndex].classList.add('active');
-
-const descripionSlider = {...document.getElementsByClassName('description-slide')};
-descripionSlider[currentIndex].classList.add('active');
